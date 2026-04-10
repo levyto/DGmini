f67eb7056eb630bf4c376029319c38ff5e446744
levyto <levy.tomas@seznam.cz>
2026-04-10 10:45:24 +0200

    Initial code import

---
+++ README.md
@@ -0,0 +1,16 @@
+# DGmini, a minimal 1D discontinuous Galerkin solver
+
+Light-weight 1D DG reference C++ implementation intended for experimentation and understanding of DG building blocks.
+
+## Build
+
+```bash
+cmake -S . -B build
+cmake --build build
+```
+
+## Run
+
+```bash
+./build/dgmini
+```
---
+++ source/main.cpp
@@ -0,0 +1,16 @@
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+//              DGmini, a minimal 1D discontinuous Galerkin solver              
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+//
+// Description: Light-weight 1D DG reference implementation intended for
+//              experimentation and understanding of DG building blocks
+//
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "DGmini: startup OK\n";
+    return 0;
+}
